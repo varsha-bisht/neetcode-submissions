e1177e7c0ff8f42b67fe27ff7ba30ae1be063b8e
Varsha Bisht <varshabisht9@gmail.com>
2026-05-03 18:52:44 +0530

    Add: python-polymorphism-with-inheritance - submission-2

---
+++ Python OOP/python-polymorphism-with-inheritance/submission-2.py
@@ -0,0 +1,29 @@
+class Animal:
+    def __init__(self, name: str):
+        self.name = name
+    
+    def make_sound(self) -> None:
+        print("Animal is making a sound")
+
+# TODO: Create the Dog and Cat classes with make_sound method
+class Dog(Animal):
+    def make_sound(self) -> None:
+        print(f"{self.name} says: Woof!")
+
+class Cat(Animal):
+    def make_sound(self) -> None:
+        print(f"{self.name} says: Meow!")
+
+def m_s(animal: Animal):
+   animal.make_sound()
+
+
+# Do not change the code below
+animal = Animal("Rabbit")
+animal.make_sound()
+
+animal = Dog("Buddy")
+animal.make_sound()
+
+animal = Cat("Whiskers")
+animal.make_sound()
