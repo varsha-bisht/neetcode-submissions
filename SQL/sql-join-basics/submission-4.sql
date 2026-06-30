e6a71da87d409c670e98091b7b4ba901dd474399
Varsha Bisht <varshabisht9@gmail.com>
2026-06-30 15:21:02 +0530

    Add: sql-join-basics - submission-4

---
+++ SQL/sql-join-basics/submission-4.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE orders (
+    order_id INTEGER PRIMARY KEY,
+    user_id INTEGER REFERENCES users(user_id),
+    total_price DECIMAL(10, 2) NOT NULL
+);
+
+INSERT INTO users (user_id, name) VALUES
+(1, 'John'),
+(2, 'Jane');
+
+INSERT INTO orders (order_id, user_id, total_price) VALUES
+(1, 1, 100.00),
+(2, 2, 200.00);
+-- Do not modify above this line. --
+
+SELECT u.user_id, u.name, o.order_id, o.total_price
+FROM users as u
+CROSS JOIN orders as o
+WHERE u.user_id = o.user_id;
+
+
+
+
