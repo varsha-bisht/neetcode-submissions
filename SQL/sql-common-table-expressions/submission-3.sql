cd43eac790d709760a28c06ba67f56791f9e535e
Varsha Bisht <varshabisht9@gmail.com>
2026-07-01 12:53:58 +0530

    Add: sql-common-table-expressions - submission-3

---
+++ SQL/sql-common-table-expressions/submission-3.sql
@@ -0,0 +1,51 @@
+CREATE TABLE customers (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT
+);
+
+CREATE TABLE orders (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  customer_id INTEGER,
+  price INTEGER
+);
+
+INSERT INTO customers (name) VALUES
+  ('Alice'),
+  ('Bob'),
+  ('Charlie'),
+  ('David'),
+  ('Eve'),
+  ('Frank'),
+  ('Grace'),
+  ('Hank');
+
+INSERT INTO orders (customer_id, price) VALUES
+  (1, 50),
+  (2, 100),
+  (3, 150),
+  (4, 200),
+  (5, 250),
+  (6, 30),
+  (7, 70),
+  (8, 400);
+-- Do not modify above this line. --
+
+
+SELECT c.name
+FROM customers AS c
+JOIN (
+  SELECT o.customer_id
+  FROM orders AS o
+  WHERE price < 100
+) AS ot ON ot.customer_id = c.id
+ORDER BY c.name;
+
+
+
+
+
+
+
+
+
+
