f6f3825a16870cdb03e9d1f18acd8da5ebb8da05
Varsha Bisht <varshabisht9@gmail.com>
2026-07-01 13:01:11 +0530

    Add: sql-common-table-expressions - submission-7

---
+++ SQL/sql-common-table-expressions/submission-7.sql
@@ -0,0 +1,53 @@
+CREATE TABLE customers (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT
+);
+
+CREATE TABLE orders (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  customer_id INTEGER,
+  price INTEGER
+);
+
+INSERT INTO customers (name) VALUES
+  ('Alice'),
+  ('Bob'),
+  ('Charlie'),
+  ('David'),
+  ('Eve'),
+  ('Frank'),
+  ('Grace'),
+  ('Hank');
+
+INSERT INTO orders (customer_id, price) VALUES
+  (1, 50),
+  (2, 100),
+  (3, 150),
+  (4, 200),
+  (5, 250),
+  (6, 30),
+  (7, 70),
+  (8, 400);
+-- Do not modify above this line. --
+
+WITH low_value_orders AS (
+   SELECT o.customer_id
+   FROM orders AS o
+   GROUP BY customer_id
+   HAVING MAX(price) < 100
+)
+
+SELECT c.name
+FROM customers AS c
+JOIN low_value_orders AS lvo ON lvo.customer_id = c.id
+ORDER BY c.name ASC;
+
+
+
+
+
+
+
+
+
+
