2104094462eb3442a8f73ec83f8b44bab40d802c
Varsha Bisht <varshabisht9@gmail.com>
2026-07-03 19:22:23 +0530

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,14 @@
+variable = 41
+print(type(variable))
+
+variable = 12.5
+print(type(variable))
+
+variable = False
+print(type(variable))
+
+variable = "hhhjb"
+print(type(variable))
+
+variable = [1,2,7,8]
+print(type(variable))
