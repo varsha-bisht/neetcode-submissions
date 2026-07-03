0dd56a66a410cfd7705a29316784e7280de9e695
Varsha Bisht <varshabisht9@gmail.com>
2026-07-03 19:20:22 +0530

    Add: python-variable-types - submission-1

---
+++ Python For Beginners/python-variable-types/submission-1.py
@@ -0,0 +1,14 @@
+integer_type = 42
+float_type = 21.5
+bool_type = True
+string_type = "Varsh"
+list_type = [1,2,3,4]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
