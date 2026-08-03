c8a347581731276e81ea967b01eb39b84eb94333
Varsha Bisht <varshabisht9@gmail.com>
2026-08-04 01:34:12 +0530

    Add: python-interface - submission-2

---
+++ Python OOP/python-interface/submission-2.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+
+class Superhero(ABC):
+    @abstractmethod
+    def fly(self) -> str:
+        pass
+
+    @abstractmethod
+    def use_power(self) -> str:
+        pass
+
+class Superman(Superhero):
+    def fly(self) -> str:
+        return "Up, up and away!"
+    
+    def use_power(self) -> str:
+        return "Using heat vision"
+
+class WonderWoman(Superhero):
+    def fly(self) -> str:
+        return "Soaring through the clouds!"
+    
+    def use_power(self) -> str:
+        return "Using lasso of truth"
+
+
+superman = Superman()
+wonder_woman = WonderWoman()
+
+print(isinstance(superman, Superhero)) 
+print(isinstance(wonder_woman, Superhero))
