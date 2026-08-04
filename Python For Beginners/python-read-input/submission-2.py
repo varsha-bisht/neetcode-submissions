476b4fc4cd23f15270f626e9a570b5919d713a7a
Varsha Bisht <varshabisht9@gmail.com>
2026-08-04 23:41:55 +0530

    Add: python-read-input - submission-2

---
+++ Python For Beginners/python-read-input/submission-2.py
@@ -0,0 +1,8 @@
+def read_and_print_input() -> str:
+    user_input  = input("Reading one line of text into a string: ")
+    return user_input
+
+print(read_and_print_input())
+print(read_and_print_input())
+
+
