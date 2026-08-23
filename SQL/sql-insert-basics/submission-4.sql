d6b39cf39b7c0cf33f700a412768512685f33f73
Varsha Bisht <varshabisht9@gmail.com>
2026-08-23 23:11:23 +0530

    Add: sql-insert-basics - submission-4

---
+++ SQL/sql-insert-basics/submission-4.sql
@@ -0,0 +1,15 @@
+CREATE TYPE account_type AS ENUM ('checking', 'savings', 'cd', 'money_market');
+
+CREATE TABLE bank_accounts (
+    id INTEGER PRIMARY KEY,
+    account_type account_type,
+    balance INTEGER
+);
+
+INSERT INTO bank_accounts (id, account_type, balance)
+   VALUES(1, 'checking', 1000),
+         (2, 'savings', 2000),
+         (3,'cd',3000),
+         (4,'money_market',4000);
+
+SELECT * FROM bank_accounts;
