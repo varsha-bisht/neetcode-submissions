4fbe57e87ea80e8c35efeab81e21a2cd10e7f3d9
Varsha Bisht <varshabisht9@gmail.com>
2026-08-24 16:43:38 +0530

    Add: sql-default-values - submission-2

---
+++ SQL/sql-default-values/submission-2.sql
@@ -0,0 +1,18 @@
+CREATE TABLE videos (
+    id INTEGER,
+    name TEXT DEFAULT 'Untitled',
+    is_published BOOLEAN DEFAULT false
+);
+
+INSERT INTO videos (id, name, is_published) 
+VALUES (1, 'My Video', true),
+       (2, 'Another Video', false);
+
+INSERT INTO videos (id)
+VALUES (3),
+       (4);
+
+INSERT INTO videos (name)
+VALUES ('Video with no ID');
+
+SELECT * FROM videos;
