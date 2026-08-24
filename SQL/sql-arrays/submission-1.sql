4dc56c10178c499d03ee102c84364f1e37c8e90a
Varsha Bisht <varshabisht9@gmail.com>
2026-08-24 18:42:51 +0530

    Add: sql-arrays - submission-1

---
+++ SQL/sql-arrays/submission-1.sql
@@ -0,0 +1,12 @@
+CREATE TABLE orders (
+    id INTEGER PRIMARY KEY,
+    items TEXT[],
+    total_price INTEGER
+);
+
+INSERT INTO orders (id, items, total_price) 
+    VALUES (1, ARRAY['apple', 'banana'], 100),
+          (2, ARRAY['orange', 'grape'], 200),
+          (3, ARRAY['watermelon', 'pineapple'], 300);
+
+SELECT * FROM orders;
