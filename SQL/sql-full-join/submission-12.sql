cc5ee2bfc6a4d9240fb5c6bb9f96e309833eee67
Varsha Bisht <varshabisht9@gmail.com>
2026-09-05 16:18:50 +0530

    Add: sql-full-join - submission-12

---
+++ SQL/sql-full-join/submission-12.sql
@@ -0,0 +1,33 @@
+CREATE TABLE pokemon_types (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    weakness TEXT
+);
+
+CREATE TABLE pokemon (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    type_id INTEGER
+);
+
+INSERT INTO pokemon_types (id, name, weakness) VALUES
+(1, 'Fire', 'Water'),
+(2, 'Water', 'Grass'),
+(3, 'Grass', 'Fire'),
+(4, 'Electric', 'Ground');
+
+INSERT INTO pokemon (id, name, type_id) VALUES
+(4, 'Charmander', 1),
+(7, 'Squirtle', 2),
+(3, 'Bulbasaur', 3),
+(147, 'Dratini', 6),
+(65, 'Alakazam', 7);
+
+SELECT pt.name as type, p.name as pokemon, pt.weakness
+FROM pokemon_types pt
+FULL JOIN pokemon p ON pt.id = p.type_id
+ORDER BY type;
+
+
+
+
