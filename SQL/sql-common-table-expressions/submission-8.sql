87af6ae2b5fa68e72de3fd8302addfff6532bb7a
Varsha Bisht <varshabisht9@gmail.com>
2026-09-08 22:20:13 +0530

    Add: sql-common-table-expressions - submission-8

---
+++ SQL/sql-common-table-expressions/submission-8.sql
@@ -0,0 +1,52 @@
+CREATE TABLE customers (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT
+);
+
+CREATE TABLE orders (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  customer_id INTEGER,
+  price INTEGER
+);
+
+INSERT INTO customers (name) VALUES
+  ('Alice'),
+  ('Bob'),
+  ('Charlie'),
+  ('David'),
+  ('Eve'),
+  ('Frank'),
+  ('Grace'),
+  ('Hank');
+
+INSERT INTO orders (customer_id, price) VALUES
+  (1, 50),
+  (2, 100),
+  (3, 150),
+  (4, 200),
+  (5, 250),
+  (6, 30),
+  (7, 70),
+  (8, 400);
+
+WITH less_value_orders AS(
+  SELECT customer_id
+  FROM orders
+  GROUP BY customer_id
+  HAVING MAX(price) < 100
+)
+
+SELECT c.name
+FROM customers c
+JOIN less_value_orders lvo ON c.id = lvo.customer_id
+ORDER BY c.name;
+
+
+
+
+
+
+
+
+
+
