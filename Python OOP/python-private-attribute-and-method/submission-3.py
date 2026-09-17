de84294a6adb9e920c3bab61639e51a9a5d11e57
Varsha Bisht <varshabisht9@gmail.com>
2026-09-17 23:51:12 +0530

    Add: python-private-attribute-and-method - submission-3

---
+++ Python OOP/python-private-attribute-and-method/submission-3.py
@@ -0,0 +1,12 @@
+class PasswordManager:
+    def __init__(self, password: str):
+        self.__password = password  
+    
+    def verify_password(self, in_pass: str) -> bool:
+        if in_pass == self.__password:
+            return True
+        return False
+
+my_password = PasswordManager("secret123")
+print(my_password.verify_password("secret123"))  # Should print: True
+print(my_password.verify_password("wrong"))      # Should print: False
